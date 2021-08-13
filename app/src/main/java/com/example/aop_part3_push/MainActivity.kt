fd9a4958bf7a4684c5fe22a32cd76d2fc00dd060
choijiwoung <choijw969133@gmail.com>
2021-08-13 18:02:11 +0900

    202108131802

--- app/src/main/java/com/example/aop_part3_push/MainActivity.kt
+++ app/src/main/java/com/example/aop_part3_push/MainActivity.kt
@@ -1,22 +1,41 @@
 package com.example.aop_part3_push
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private val resultTextView: TextView by lazy {
-        findViewById(R.id.resultTextView)
-    }
+/*       private val resultTextView: TextView by lazy {
+            findViewById(R.id.resultTextView)
+        }
 
-    private val firebaseToken: TextView by lazy {
-        findViewById(R.id.firebaseTokenTextView)
+        private val firebaseToken: TextView by lazy {
+            findViewById(R.id.firebaseTokenTextView)
+        }*/
+    private val parentId: LinearLayout by lazy {
+        findViewById(R.id.parentId)
     }
-
+    private var mFixedLayoutContainerView: childView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        initView()
+        mFixedLayoutContainerView?.scaleX=50.5f
+        mFixedLayoutContainerView?.scaleY=18.5f
+    }
+
+    private fun initView() {
+        mFixedLayoutContainerView = childView(
+            this,
+            100,
+            100
+        )
+        parentId.addView(mFixedLayoutContainerView)
+
     }
 }
