1938f9983f6f90ab722e9977a5b5f056620e5623
choijiwoung <choijw969133@gmail.com>
2021-08-18 19:00:52 +0900

    202108181900

--- app/src/main/java/com/example/aop_part3_push/MainActivity.kt
+++ app/src/main/java/com/example/aop_part3_push/MainActivity.kt
@@ -1,41 +1,58 @@
 package com.example.aop_part3_push
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingService
 
 class MainActivity : AppCompatActivity() {
 
-/*       private val resultTextView: TextView by lazy {
-            findViewById(R.id.resultTextView)
-        }
-
-        private val firebaseToken: TextView by lazy {
-            findViewById(R.id.firebaseTokenTextView)
-        }*/
-    private val parentId: LinearLayout by lazy {
-        findViewById(R.id.parentId)
+    private val resultTextView: TextView by lazy {
+        findViewById(R.id.resultTextView)
     }
-    private var mFixedLayoutContainerView: childView? = null
+
+    private val firebaseToken: TextView by lazy {
+        findViewById(R.id.firebaseTokenTextView)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        initView()
-        mFixedLayoutContainerView?.scaleX=50.5f
-        mFixedLayoutContainerView?.scaleY=18.5f
+        initFirebase()
+        updateResult()
     }
 
-    private fun initView() {
-        mFixedLayoutContainerView = childView(
-            this,
-            100,
-            100
-        )
-        parentId.addView(mFixedLayoutContainerView)
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        setIntent(intent) //이거 안해주면 데이터 안들어옴
+        updateResult(true)
 
     }
+
+    private fun initFirebase() {
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    firebaseToken.text = task.result
+                }
+            }
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    private fun updateResult(isNewIntent: Boolean = false) {
+        resultTextView.text =
+            (intent.getStringExtra("notificationType") ?: "앱 런처") + if (isNewIntent) {
+                "로 갱신 했습니다."
+            } else {
+                "로 실행 했습니다."
+            }
+    }
+
 }
